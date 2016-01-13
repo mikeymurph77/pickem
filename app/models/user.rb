6bda2bec4e30fcb7573f383b441cdc2ad1a9bc31
Michael Murphy <mikeymurph77@gmail.com>
2016-01-12 21:55:34 -0500

    validate presence true for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
+  validates :username, presence: true
 
   def name
     "#{first_name} #{last_name}"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,12 @@ describe User do
     expect(user).to be_valid
   end
 
+  it "is invalid without a username" do
+    user = User.new(username: nil)
+    user.valid?
+    expect(user.errors[:username]).to include("can't be blank")
+  end
+
   it "is invalid without an email address" do
     user = User.new(email: nil)
     user.valid?
