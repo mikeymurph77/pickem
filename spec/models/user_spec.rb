8ea1c9dd9dd3ec318e723312378ab3886e1fddb0
Michael Murphy <mikeymurph77@gmail.com>
2016-01-17 16:53:34 -0500

    Remove duplicate validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,18 +3,6 @@ require 'rails_helper'
 describe User do
   subject { User.new(email: 'foo@bar.com', password_digest: 'password', username: 'foo') }
 
-  it "is valid with a username, first_name, last_name, password and email" do
-    user = User.new(
-      username: "Bob77",
-      first_name: "Bob",
-      last_name: "Willson",
-      password_digest: "abcd1234",
-      email: "bob77@testemail.com"
-    )
-
-    expect(user).to be_valid
-  end
-
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password_digest) }
