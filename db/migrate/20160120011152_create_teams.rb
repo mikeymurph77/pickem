9c708ba4f9194d859172992352a315c3063b9000
Michael Murphy <mikeymurph77@gmail.com>
2016-01-19 20:17:42 -0500

    Create Teams with migration

---
+++ db/migrate/20160120011152_create_teams.rb
@@ -0,0 +1,12 @@
+class CreateTeams < ActiveRecord::Migration
+  def change
+    create_table :teams do |t|
+      t.string :name, null: false
+      t.string :city, null: false
+
+      t.timestamps null: false
+    end
+
+    add_index :teams, :name, unique: true
+  end
+end
