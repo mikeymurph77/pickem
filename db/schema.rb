ecc0877c593c427437e635fbf0f92b6c58207217
Michael Murphy <mikeymurph77@gmail.com>
2016-02-15 15:32:04 -0500

    Run Week migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127035213) do
+ActiveRecord::Schema.define(version: 20160127035545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,5 +52,9 @@ ActiveRecord::Schema.define(version: 20160127035213) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
+
+  create_table "weeks", force: :cascade do |t|
+    t.datetime "start_date", null: false
+  end
 
 end
